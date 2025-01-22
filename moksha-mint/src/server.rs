f5244a5387955e64c24f98aec5cc51f33c89012c
MT <tymkomykyta2108@gmail.com>
2025-01-22 17:13:11 +0100

    Add old functions back.

--- moksha-mint/src/server.rs
+++ moksha-mint/src/server.rs
@@ -3,13 +3,7 @@ use crate::routes::btconchain::{
     post_melt_btconchain, post_melt_quote_btconchain, post_mint_btconchain,
     post_mint_quote_btconchain,
 };
-use crate::routes::default::{
-    check_bitcredit_quote, get_info, get_keys, get_keys_by_id, get_keysets, get_keysets_by_id,
-    get_melt_quote_bolt11, get_mint_quote_bitcredit, get_mint_quote_bolt11, post_melt_bolt11,
-    post_melt_quote_bolt11, post_mint_bitcredit, post_mint_bolt11, post_mint_quote_bitcredit,
-    post_mint_quote_bolt11, post_request_to_mint_bitcredit, post_swap,
-    mjk_get_info, mjk_get_keysets, mjk_get_keys, mjk_get_keys_by_id, mjk_post_swap
-};
+use crate::routes::default::{check_bitcredit_quote, get_info, get_keys, get_keys_by_id, get_keysets, get_keysets_by_id, get_melt_quote_bolt11, get_mint_quote_bitcredit, get_mint_quote_bolt11, post_melt_bolt11, post_melt_quote_bolt11, post_mint_bitcredit, post_mint_bolt11, post_mint_quote_bitcredit, post_mint_quote_bolt11, post_request_to_mint_bitcredit, post_swap, mjk_get_info, mjk_get_keysets, mjk_get_keys, mjk_get_keys_by_id, mjk_post_swap, get_keysets_old, get_keys_old};
 use axum::extract::Request;
 use axum::http::{HeaderName, HeaderValue, StatusCode};
 use axum::middleware::Next;
@@ -184,8 +178,10 @@ fn app(mint: Mint) -> Router {
         .route("/:id/:unit/v1/swap", post(mjk_post_swap))
         /* mjk - end */
         .route("/v1/keys/:unit", get(get_keys))
+        .route("/v1/keys", get(get_keys_old))
         .route("/v1/keys/:id/:unit", get(get_keys_by_id))
         .route("/v1/keysets/:unit", get(get_keysets))
+        .route("/v1/keysets", get(get_keysets_old))
         .route("/v1/keysets/:unit/:id", get(get_keysets_by_id))
         .route("/v1/mint/quote/bolt11", post(post_mint_quote_bolt11))
         .route("/v1/mint/quote/bitcredit", post(post_mint_quote_bitcredit))
